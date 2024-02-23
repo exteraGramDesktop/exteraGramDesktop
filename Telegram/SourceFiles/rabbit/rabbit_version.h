93d5003f66209db29940e6c9e160725856ddae3b
xmdn <72883689+xmdnx@users.noreply.github.com>
2024-02-23 17:04:02 +0300

    v.4.15

--- Telegram/SourceFiles/rabbit/rabbit_version.h
+++ Telegram/SourceFiles/rabbit/rabbit_version.h
@@ -7,6 +7,6 @@ https://github.com/rabbitgramdesktop/rabbitgramdesktop/blob/dev/LEGAL
 */
 #pragma once
 
-constexpr auto AppRabbitVersion = 4014000;
-constexpr auto AppRabbitVersionStr = "4.14";
+constexpr auto AppRabbitVersion = 4015000;
+constexpr auto AppRabbitVersionStr = "4.15";
 constexpr auto AppRabbitBetaVersion = false;
